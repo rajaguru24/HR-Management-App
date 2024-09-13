69a04deb739ce23caaacaa9fd21c60c9b0d4806f
rajaguru24 <rajaguru246@gmail.com>
2024-09-13 11:26:47 +0530

    updated

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://hr-management-app-1.onrender.com";
 export const GetAllEmployees = async (search = "", page = 1, limit = 5) => {
   const url = `${BASE_URL}/api/employees?search=${search}&page=${page}&limit=${limit}`;
   try {
