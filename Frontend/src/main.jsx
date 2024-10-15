7dd9a27fc3a142308dbc9e34839b3d4630f2cfac
rajaguru24 <rajaguru246@gmail.com>
2024-10-15 21:44:08 +0530

    update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
+import axios from "axios";
+axios.defaults.baseURL = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
