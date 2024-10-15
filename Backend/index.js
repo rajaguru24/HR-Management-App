2c859c04c2310efd717c0395bd7932c112da9872
rajaguru24 <rajaguru246@gmail.com>
2024-10-15 18:44:45 +0530

    cors update

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 require("dotenv").config();
-var cors = require("cors");
-const corsOptions = {
-  origin: 'https://celadon-malasada-d4eb0d.netlify.app/resetpassword/:token',//(https://your-client-app.com)
-  optionsSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://celadon-malasada-d4eb0d.netlify.app/",
+    ],
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 require("./Models/db");
 app.use(bodyParser.json());
 
-const employeeRouter = require("./Routes/employeeRoutes");  
-const userRouter = require("./Routes/userRoutes");  
+const employeeRouter = require("./Routes/employeeRoutes");
+const userRouter = require("./Routes/userRoutes");
 
 app.get("/", (req, res) => {
-  res.send("Employee Management server is running"); 
+  res.send("Employee Management server is running");
 });
 app.use("/api", employeeRouter);
 app.use("/api/user", userRouter);
-
 
 app.listen(PORT, () => {
   console.log("server is running on PORT", PORT);
