763298c70e5cc966eece1bba8dbeba4c0ffe3ab9
rajaguru24 <rajaguru246@gmail.com>
2024-10-15 19:29:09 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 
 app.use(cors({
   origin: 'https://hr-management-app-brown.vercel.app/', // use your actual domain name (or localhost), using * is not recommended
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin, Content-Type, X-Auth-Token'],
   credentials: true
 }))
 
