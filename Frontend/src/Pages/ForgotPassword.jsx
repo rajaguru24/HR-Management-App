dff333e6b816713b72dc969d5a4b1f891f97128e
rajaguru24 <rajaguru246@gmail.com>
2024-10-15 21:55:49 +0530

    update

--- Frontend/src/Pages/ForgotPassword.jsx
+++ Frontend/src/Pages/ForgotPassword.jsx
@@ -12,7 +12,7 @@ const ForgotPassword = () => {
     console.log("hello");
     try {
       const response = await axios.post(
-        "https://hr-management-app.onrender.com/api/user/ForgotPassword",
+        "https://hr-management-app-brown.vercel.app/api/user/ForgotPassword",
         { email }
       );
       console.log(response);
